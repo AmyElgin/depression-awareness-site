e7a7951175640da9637cc4e1bf4b2dd3ac183396
AmyElgin <28elgamy@stmarysdsg.co.za>
2025-05-10 12:42:53 +0200

    Add files via upload

--- chat.js
+++ chat.js
@@ -1,27 +1,26 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js';
-import { getDatabase, ref, push } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js';
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyB8RPTDWCPMRLl-IirhunsbBvCw-PRytVs",
-    authDomain: "depression-b81de.firebaseapp.com",
-    projectId: "depression-b81de",
-    storageBucket: "depression-b81de.firebasestorage.app",
-    messagingSenderId: "206001450588",
-    appId: "1:206001450588:web:4259c6db7d8c40894fc099",
-    measurementId: "G-M58XNP9P8H"
-  };
+  apiKey: "AIzaSyB8RPTDWCPMRLl-IirhunsbBvCw-PRytVs",
+  authDomain: "depression-b81de.firebaseapp.com",
+  projectId: "depression-b81de",
+  storageBucket: "depression-b81de.firebasestorage.app",
+  messagingSenderId: "206001450588",
+  appId: "1:206001450588:web:4259c6db7d8c40894fc099",
+  measurementId: "G-M58XNP9P8H"
+};
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-// Start chat
 window.startChat = function () {
   const chatContainer = document.getElementById('chat-container');
   chatContainer.innerHTML = `
-    <p>Start chatting with someone anonymously</p>
-    <textarea id="chatMessage" placeholder="Type your message here..."></textarea>
+    <p>Start chatting anonymously</p>
+    <textarea id="chatMessage" placeholder="Type your message..."></textarea>
     <button onclick="sendMessage()">Send</button>
   `;
 };
@@ -29,13 +28,7 @@ window.startChat = function () {
 window.sendMessage = function () {
   const message = document.getElementById('chatMessage').value;
   const messagesRef = ref(db, 'chat/messages');
-
-  push(messagesRef, {
-    message: message,
-    timestamp: Date.now()
-  }).then(() => {
-    alert("Message sent!");
-  }).catch((error) => {
-    console.error("Error sending message: ", error);
-  });
+  push(messagesRef, { message: message, timestamp: Date.now() })
+    .then(() => alert("Message sent!"))
+    .catch((error) => console.error("Error:", error));
 };
--- quiz.js
+++ quiz.js
@@ -1,22 +1,21 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js';
-import { getDatabase, ref, set } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js';
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyB8RPTDWCPMRLl-IirhunsbBvCw-PRytVs",
-    authDomain: "depression-b81de.firebaseapp.com",
-    projectId: "depression-b81de",
-    storageBucket: "depression-b81de.firebasestorage.app",
-    messagingSenderId: "206001450588",
-    appId: "1:206001450588:web:4259c6db7d8c40894fc099",
-    measurementId: "G-M58XNP9P8H"
-  };
+  apiKey: "AIzaSyB8RPTDWCPMRLl-IirhunsbBvCw-PRytVs",
+  authDomain: "depression-b81de.firebaseapp.com",
+  projectId: "depression-b81de",
+  storageBucket: "depression-b81de.firebasestorage.app",
+  messagingSenderId: "206001450588",
+  appId: "1:206001450588:web:4259c6db7d8c40894fc099",
+  measurementId: "G-M58XNP9P8H"
+};
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-// Start quiz
 window.startQuiz = function () {
   const quizContainer = document.getElementById('quiz-container');
   quizContainer.innerHTML = `
@@ -29,12 +28,7 @@ window.startQuiz = function () {
 window.submitQuiz = function () {
   const response = document.getElementById('response').value;
   const newRef = ref(db, 'quiz/responses/' + Date.now());
-
-  set(newRef, {
-    response: response
-  }).then(() => {
-    alert("Your response has been saved!");
-  }).catch((error) => {
-    console.error("Error saving response: ", error);
-  });
+  set(newRef, { response: response })
+    .then(() => alert("Your response has been saved!"))
+    .catch((error) => console.error("Error saving response:", error));
 };
